b8faf3ae8c296c1c8b79c98b3b30278745af54dc
fredie04 <me@fredie04.dev>
2024-02-26 07:15:47 +0100

    Remove redudant tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,9 @@ pub mod bowl;
 pub mod oat;
 pub mod region;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     mod bowl {
         use crate::bowl::{Bowl, GenerationBehavior, WrappedBowl};
